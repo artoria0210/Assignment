a282e2633be5183db6e535cd1b687b4d6596d811
artoria0210 <66988774+artoria0210@users.noreply.github.com>
2020-08-25 13:46:58 +0800

    Commit by Chua Guangwen

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Password;
+    private EditText Phone_Number;
+    private EditText Confirm_Password;
+    private Button login;
+    private Button register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Password = (EditText) findViewById(R.id.editTextTextPassword);
+        Password = (EditText) findViewById(R.id.editTextTextPassword2);
+        Password = (EditText) findViewById(R.id.editTextTextPassword3);
+        Phone_Number = (EditText) findViewById(R.id.editTextPhone);
+        Phone_Number = (EditText) findViewById(R.id.editTextPhone2);
+        login = (Button) findViewById(R.id.button);
+        register = (Button) findViewById(R.id.button);
+
+
     }
+
+
+
 }
